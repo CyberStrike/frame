61edbf3ab192bababcae5c21517ee78d3b3aeaf6
Chris Scott <cyberstrike@gmail.com>
2017-10-02 02:42:10 -0400

    added photos to index

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const multer  = require('multer')
 const fs      = require('fs')
 const Loki    = require('lokijs')
 const logger  = require('morgan');
+const mime    = require('mime-types').extension
 
 // Utility
 const util    = require('./utils')
@@ -15,8 +16,17 @@ const util    = require('./utils')
 const DB_NAME         = 'db.json';
 const COLLECTION_NAME = 'images';
 const UPLOAD_PATH     = 'uploads';
-const upload          = multer({ dest: `${UPLOAD_PATH}/` }); // multer configuration
 const DB              = new Loki(`${UPLOAD_PATH}/${DB_NAME}`, { persistenceMethod: 'fs' });
+const storage         = multer.diskStorage({
+                          destination: function (req, file, cb) {
+                            cb(null,  `${UPLOAD_PATH}/`)
+                          },
+                          filename: function (req, file, cb) {
+                            const extension = mime(file.mimetype)
+                            cb(null, Date.now() + `.${extension}`)
+                          }
+                        })
+const upload          = multer({ storage: storage , fileFilter: util.imageFilter });
 
 const app = express();
 
@@ -30,6 +40,9 @@ app.set('view engine', 'pug');
 //
 // Middleware Setup
 //
+
+// Serve Image Files
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
 // Logger Setup
 app.use(logger('dev'));
@@ -46,9 +59,12 @@ app.use( function(req, res, next) {
 //
 
 app.get('/', async (req, res) => {
-
-  res.render('index', {title: 'Frame', photos: [1,2,3]})
-
+  try {
+    const col = await util.loadCollection(COLLECTION_NAME, DB);
+    res.render('index', {title: 'Frame', photos: col.data})
+  } catch (err) {
+    res.sendStatus(400);
+  }
 })
 
 app.get('/photos/new', function (req, res) {
@@ -57,13 +73,18 @@ app.get('/photos/new', function (req, res) {
 
 app.post('/photos/create', upload.single('photo'), async (req, res) => {
   try {
+    if ( !req.file ) {
+      console.log('No Upload Found.')
+      return res.sendStatus(400);
+    }
 
     const col = await util.loadCollection(COLLECTION_NAME, DB);
     const data = col.insert(req.file);
-    
+
     DB.saveDatabase();
 
     res.redirect(`/photo/${data.$loki}`)
+    // res.redirect('/')
 
   } catch (err) {
     res.sendStatus(400);
@@ -89,24 +110,23 @@ app.get('/photo/:id', async (req, res) => {
 // Ensure 404 and Show Errors
 //
 
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   let err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-//
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 // Init Server
 app.listen(3000, function (err) {
   if (err) throw err;
